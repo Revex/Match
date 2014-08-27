da8ca49442e296013c773e1393dbf5ad183a4b05
X <mr.hardy@gmail.com>
2014-08-27 12:30:05 -0500

    fixed bug with editing the name of items deleting the notes

--- client/item/itemEdit/itemEdit.js
+++ client/item/itemEdit/itemEdit.js
@@ -3,7 +3,13 @@ Template.itemEdit.events({
         // the submit button pressed
         var itemName = $("#itemName").val();
         var itemId = $("#itemId").val();
-        Items.update( { _id : itemId }, { name : itemName} );
+        Items.update({
+            _id: itemId
+        }, {
+            $set: {
+                name: itemName
+            }
+        });
 
         Router.go('itemList');
     }
